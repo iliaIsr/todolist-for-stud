86519398e31fde0ebc556c2a05d20eb077cc2fdd
iliaIsr <ilil.sbl91@gmail.com>
2024-06-20 15:55:43 +0300

    1.07

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TasksType, Todolist} from "./Todolist";
+
 
 function App() {
+    let [tasks,setTasks]=useState([
+        {id:1, title:"CSS", isDone:true},
+        {id:2, title:"JS", isDone:true},
+        {id:3, title:"React", isDone:false},
+        {id:4, title:"Redax", isDone:false},
+    ])
+    let [filter, setFilter]=useState('all')
+
+    function removeTask(id:number){
+      let filteredTasks=tasks.filter(t=>t.id!==id)
+        setTasks(filteredTasks)
+    }
+    function filterTasks(filteredTask:[]){
+        let filters=filteredTask,
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todolist title={"What to lern"}
+                tasks={tasks}
+                removeTask={removeTask}
+
+      />
     </div>
   );
 }
